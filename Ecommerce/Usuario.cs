43d9af03ca28b8aa172c72e332040b84b6694753
Esteban Raffo <e.fraffo@gmail.com>
2024-05-11 17:10:01 -0300

    Primeros pasos

---
+++ Ecommerce/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce
+{
+    public class Usuario
+    {
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+        private string _nombreUsuario;
+        private string _password;
+        private string _mail;
+
+        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string password, string mail)
+        {
+            this._id = id;
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._nombreUsuario = nombreUsuario;
+            this._password = password;
+            this._mail = mail;
+        }
+    }
+}
---
+++ Ecommerce/Venta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce
+{
+    public class Venta
+    {
+        private int _id;
+        private string _comentarios;
+        private int _idUsuario;
+
+        public Venta(int id, string comentarios, int idUsuario)
+        {
+            this._id = id;
+            this._comentarios = comentarios;
+            this._idUsuario = idUsuario;
+        }
+    }
+}
